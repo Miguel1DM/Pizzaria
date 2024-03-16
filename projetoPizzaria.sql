345ef1680299fb624a4aa6007e81bb9d7e0930ab
Miguel Pereira <141651248+Miguel1DM@users.noreply.github.com>
2024-03-16 19:30:45 -0300

    Update projetoPizzaria.sql

--- projetoPizzaria.sql
+++ projetoPizzaria.sql
@@ -1,45 +1,83 @@
+CREATE DATABASE Pizzaria;
+
+USE Pizzaria;
+
 CREATE TABLE Cliente(
 	nome VARCHAR(25) NOT NULL,
-    	cep varchar(10) NOT NULL,
-    	endereco VARCHAR(50) NOT NULL,
-    	id SMALLINT PRIMARY KEY AUTO_INCREMENT,
-    	telefone VARCHAR(15) NOT NULL
+    cep varchar(10) NOT NULL,
+    endereco VARCHAR(50) NOT NULL,
+    id SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    telefone VARCHAR(15) NOT NULL
     
 );
 
 CREATE TABLE Pizzas(
 	id SMALLINT KEY AUTO_INCREMENT,
-    	nome VARCHAR(15) NOT NULL,
-    	tempoPreparo VARCHAR(5) NOT NULL,
-    	ingredientes VARCHAR(150) NOT NULL    
-);
-
-CREATE TABLE Estoque(
-	id SMALLINT KEY AUTO_INCREMENT,
-    	nomeIngrediente VARCHAR(20) NOT NULL,
-    	quantidade MEDIUMINT NOT NULL
+    nome VARCHAR(15) NOT NULL,
+    preco FLOAT NOT NULL,
+    tempoPreparo VARCHAR(5) NOT NULL,
+    idSabores SMALLINT,
+	idIngredientes SMALLINT,
+    idBorda SMALLINT,
+    idAdicionais SMALLINT,
+    FOREIGN KEY (idSabores) REFERENCES Sabores (id),
+	FOREIGN KEY (idIngredientes) REFERENCES Ingredientes (id),
+    FOREIGN KEY (idAdicionais) REFERENCES Adicionais (id),
+    FOREIGN KEY (idBorda) REFERENCES Bordas (id)
 );
 
 CREATE TABLE Pedidos(
 	id SMALLINT KEY AUTO_INCREMENT,
 	data DATETIME,
-    	idPizza SMALLINT,
-    	idCliente SMALLINT,
-	FOREIGN KEY (idPizza) REFERENCES Pizzas (id),
-    	FOREIGN KEY (idCliente) REFERENCES Cliente (id)
+	idCliente SMALLINT,
+    idPizzas SMALLINT,
+    idBebida SMALLINT,
+	FOREIGN KEY (idPizzas) REFERENCES Pizzas (id),
+    FOREIGN KEY (idBebida) REFERENCES Bebidas (id),
+    FOREIGN KEY (idCliente) REFERENCES Cliente (id)
 );
 
 CREATE TABLE Fornadas(
-	idPizza SMALLINT,
-    	idCliente SMALLINT,
-    	dataHora DATETIME,
-	FOREIGN KEY (idPizza) REFERENCES Pizzas(id),
-    	FOREIGN KEY (idCliente) REFERENCES Cliente(id)
+	idPedido SMALLINT,
+    dataHora DATETIME,
+	FOREIGN KEY (idPedido) REFERENCES Pedidos(id)
 );
 
+CREATE TABLE Bebidas(
+	id SMALLINT KEY AUTO_INCREMENT,
+    nome VARCHAR(15) NOT NULL,
+    descricao VARCHAR(50) NOT NULL,
+    preco FLOAT NOT NULL,
+    idIngredientes SMALLINT,
+    FOREIGN KEY (idIngredientes) REFERENCES Ingredientes (id)
+);
 
+CREATE TABLE Bordas(
+	id SMALLINT KEY AUTO_INCREMENT,
+    nome VARCHAR(20) NOT NULL,
+    preco FLOAT NOT NULL,
+	idIngredientes SMALLINT,
+    FOREIGN KEY (idIngredientes) REFERENCES Ingredientes (id)
+);
 
+CREATE TABLE Adicionais(
+	id SMALLINT KEY AUTO_INCREMENT,
+    nome VARCHAR(20) NOT NULL,
+    preco FLOAT NOT NULL,
+    dIngredientes SMALLINT,
+    FOREIGN KEY (idIngredientes) REFERENCES Ingredientes (id)
+);
 
+CREATE TABLE Sabores(
+	id SMALLINT KEY AUTO_INCREMENT,
+	tipoSabor Varchar(20) NOT NULL,
+	idIngredientes SMALLINT,
+    FOREIGN KEY (idIngredientes) REFERENCES Ingredientes(id)
+);
 
-
+CREATE TABLE Ingredientes(
+	id SMALLINT KEY AUTO_INCREMENT,
+    nome VARCHAR(15),
+    preco FLOAT
+);
 
